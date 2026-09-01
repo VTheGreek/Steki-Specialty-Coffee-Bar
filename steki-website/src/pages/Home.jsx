00be325a0d466542a4cba6e6a5ea9fda79707545
VTheGreek <vasilis.proefth@gmail.com>
2026-09-02 00:21:04 +0300

    feat: create new website structure

--- steki-website/src/pages/Home.jsx
+++ steki-website/src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import FeatureBanner from "../components/FeatureBanner";
 import About from "../components/About";
-import CoffeeSection from "../components/CoffeeSection";
-import FoodSection from "../components/FoodSection";
 import Footer from "../components/Footer";
-import Menu from "../components/Menu";
 
 function Home() {
   return (
@@ -14,11 +10,7 @@ function Home() {
 
       <main>
         <Hero />
-        <FeatureBanner />
         <About />
-        <Menu />
-        <CoffeeSection />
-        <FoodSection />
       </main>
 
       <Footer />
